9d8ff731ff58d602cc2b732eeb2095248183ee1c
original <git@github.com:denis-poroshin/variables.git>
2024-02-13 20:31:04 +0300

    Исправил домашнюю работу.

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,78 @@ public class Main {
         double [] fractionalNumbers = {1.57, 7.654, 9.986};
         int [] anyWholeNumbers = {4, 5, 6};
         System.out.println("Задание 2");
-        System.out.println(wholeNumbers[0] + ", " + wholeNumbers[1] + ", " + wholeNumbers[2]);
-        System.out.println(fractionalNumbers[0] + ", " + fractionalNumbers[1] + ", " + fractionalNumbers[2]);
-        System.out.println(anyWholeNumbers[0] + ", " + anyWholeNumbers[1] + ", " + anyWholeNumbers[2]);
-        System.out.println("Задание 3");
-        System.out.println(wholeNumbers[2] + ", " + wholeNumbers[1] + ", " + wholeNumbers[0]);
-        System.out.println(fractionalNumbers[2] + ", " + fractionalNumbers[1] + ", " + fractionalNumbers[0]);
-        System.out.println(anyWholeNumbers[2] + ", " + anyWholeNumbers[1] + ", " + anyWholeNumbers[0]);
-        System.out.println("Задание 4");
+        for (int i = 0; i < wholeNumbers.length; i++)
+        {
+            if (i == wholeNumbers.length -1)
+            {
+                System.out.print(wholeNumbers[i]);
+            }
+            else
+            {
+                System.out.print(wholeNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < fractionalNumbers.length; i++)
+        {
+            if (i == fractionalNumbers.length -1)
+            {
+                System.out.print(fractionalNumbers[i]);
+            }
+            else
+            {
+                System.out.print(fractionalNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < anyWholeNumbers.length; i++)
+        {
+            if (i == anyWholeNumbers.length -1)
+            {
+                System.out.print(anyWholeNumbers[i]);
+            }
+            else
+            {
+                System.out.print(anyWholeNumbers[i] + ", ");
+            }
+        }
+        System.out.println("\nЗадание 3");
+        for (int i = wholeNumbers.length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                System.out.print(wholeNumbers[i]);
+            }
+            else
+            {
+                System.out.print(wholeNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int i = fractionalNumbers.length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                System.out.print(fractionalNumbers[i]);
+            }
+            else
+            {
+                System.out.print(fractionalNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int i = anyWholeNumbers.length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                System.out.print(anyWholeNumbers[i]);
+            }
+            else
+            {
+                System.out.print(anyWholeNumbers[i] + ", ");
+            }
+        }
+        System.out.println("\nЗадание 4");
         for (int i = 0; i < wholeNumbers.length; i++)
         {
             if (wholeNumbers[i] % 2 != 0)
